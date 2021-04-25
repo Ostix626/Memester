e627ee605c79090bb3f95eaf01e7d6a7e6ac5e75
Ostix626 <martinluk.luk@gmail.com>
2021-04-25 03:16:10 +0200

    removed build in commit

--- app/src/main/java/com/memester/BottomSheetFragment.kt
+++ app/src/main/java/com/memester/BottomSheetFragment.kt
@@ -1,29 +1,47 @@
 package com.memester
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.bottomsheet_fragment.*
 
 class BottomSheetFragment: BottomSheetDialogFragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+
+        val view = inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        change_profile_pic_btn.setOnClickListener {
-            Toast.makeText(context, "TODO: implement change profile pic", Toast.LENGTH_LONG).show()
+        edit_profile_btn.setOnClickListener {
+            //Toast.makeText(context, "TODO: implement change profile pic", Toast.LENGTH_LONG).show()
+            val intent = Intent(context, AccountSettingsActivity::class.java)
+            startActivity(intent);
         }
 
         logout_btn.setOnClickListener {
--- app/src/main/java/com/memester/ProfileFragment.kt
+++ app/src/main/java/com/memester/ProfileFragment.kt
@@ -1,20 +1,16 @@
 package com.memester
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
 import com.memester.Model.User
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
     private lateinit var db: DatabaseReference
